ff5ccb7bc0b337da4df1974b33f00c8c34cef1f4
rohmad-st <rochmad.26@gmail.com>
2015-10-12 14:22:34 +0700

    Multiple add and update
    - update fungsi kop surat

--- inc/LetterHead.php
+++ inc/LetterHead.php
@@ -3,63 +3,51 @@ require('../lib/fpdf.php');
 
 class LetterHead extends FPDF
 {
-    // variabel menentukan proses ke
-    var $numproc = 1;
+    var $widths;
 
-    function addLetterHead($x, $y, $width, $row, array $data)
+    /**
+     * Definite width column
+     *
+     * @param $w
+     */
+    function SetWidths($w)
     {
-        $x = $x + 30;
-        $height = $row * 8;
-
-        // box image
-        if ($this->numproc == 1) {
-            $curx = $this->GetX();
-            $cury = $this->GetY();
-            $this->Image("http://placehold.it/100x129.jpg", $curx + 3, $cury + 1, 30 - 6, $height - 2);
-            $this->Rect($curx, $cury, 30, $height);
-        }
-
-        // box letter head
-        $this->Rect($x, $y, $width, $height);
-
-        for ($i = 0; $i < count($data); $i ++) {
-            $this->SetXY($x, $y);
-            $this->MultiCell($width, 5, $data[$i], 0, 'C');
-            $y += 5;
-        }
-
-        $this->numproc ++;
+        //Set the array of column widths
+        $this->widths = $w;
     }
 
-    function addLH(array $data)
+    /**
+     * Add Letter Head (Kop Surat) exactly in top page
+     *
+     * @param array $data
+     * @param null  $file
+     */
+    function AddLetterHead(array $data, $file = null)
     {
-//        $r1 = $this->GetX();
-//        $r2 = $this->w - ($r1 * 2);
-//        $y1 = $this->GetY() ;
-//        $y2 = $this->heights - 50 - $y1;
-//
-//        $this->SetXY($r1, $y1);
-//        $this->Rect($r1, $y1, $r2, $y2, "D");
+        $length = count($data); // length data
+        $column = ($this->FontSizePt / 2) - 1; // height of row
 
-//        // jumlah array
-        $length = count($data);
-        $x = $this->GetX();
-        $y = $this->GetY();
+        $x = $this->lMargin; // set x position exactly on left margin
+        $y = $this->tMargin; // set y position exactly on top margin
+        $w = $this->widths; // width column
+        $h = ($length * $column) + 4; // height box
 
-        $width_sum = 0;
-        for ($i = 0; $i < $length; $i ++) {
-            $width_sum += $this->widths[$i];
+        $this->Rect($x, $y, $w + 25, $h); // box in out of text
+
+        // check if image is valid as image type
+        if (exif_imagetype($file) == false) {
+            $file = '../inc/logo.png';
         }
 
-        $this->Rect($x, $y, $width_sum, 30, 'D');
-//
+        $this->Image($file, $x + 5, $y + 1, 0, $h - 2); // image of head letter
+
+        // text in letter head
+        $this->SetY($y + 2);
         for ($i = 0; $i < $length; $i ++) {
-            $width = $this->widths[$i];
-            $this->Cell($width, 4, $data[$i], 0, 0, 'C');
-            $x += $width;
+            $this->SetX($x + 25);
+            $this->Cell($w, $column, $data[$i], 0, 0, 'C');
+            $this->Ln($column);
         }
-//
-        $this->Ln(4);
     }
 
 }
